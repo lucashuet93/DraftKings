99331c310ccefda82ef751fdd1047b05b3634c73
Lucas Huet-Hudson <lucas.huet.hudson@gmail.com>
2020-09-07 14:42:07 -0400

    add arm template and sql file for sql deployment

---
+++ deployment/create-tables.sql
@@ -0,0 +1,78 @@
+CREATE TABLE DraftKingsAvailables (
+    Position varchar(255),
+    FullNamePlayerIdCombination varchar(255),
+    FullName varchar(255),
+    PlayerId varchar(255),
+    RosterPosition varchar(255),
+    Salary int,
+    GameInfo varchar(255),
+    Team varchar(255),
+    AveragePPG float
+);
+
+CREATE TABLE NumberFire (
+    FullNameAndPosition varchar(255),
+    ProjectedPoints float,
+    Salary int,
+    ProjectedValue float,
+    CompletionsOverAttempts varchar(255),
+    PassingYds int,
+    PassingTds int,
+    PassingInts int,
+    RushingAtt int,
+    RushingYds int,
+    RushingTds int,
+    Receptions int,
+    ReceivingYds int,
+    ReceivingTds int,
+    Targets float
+);
+
+CREATE TABLE DailyFantasyFuel (
+    FirstName varchar(255),
+    LastName varchar(255),
+    Position varchar(255),
+    InjuryStatus varchar(255),
+    GameDate varchar(255),
+    Slate varchar(255),
+    Team varchar(255),
+    Opponent varchar(255),
+    Spread float,
+    OverUnder float,
+    ImpliedTeamScore float,
+    Salary int,
+    Last5DvPRank int,
+    Last5PPGFloor float,
+    Last5PPGAverage float,
+    Last5PPGCeiling float,
+    ProjectedPoints float,
+    ProjectedValue float,
+    ActualPoints float,
+    ActualValue float
+);
+
+CREATE TABLE RotoGrinders (
+    FullName varchar(255),
+    Salary varchar(255),
+    Team varchar(255),
+    Position varchar(255),
+    Opponent varchar(255),
+    ProjectedPoints float,
+    ProjectedValue float
+);
+
+CREATE TABLE PlayerProjections (
+    FirstName varchar(255),
+    LastName varchar(255),
+    PlayerId varchar(255),
+    Position varchar(255),
+    Salary int,
+    Team varchar(255),
+    Opponent varchar(255),
+    RotoGrindersProjection float,
+    NumberFireProjection float,
+    DailyFantasyFuelProjection float,
+    ProjectedPoints float,
+    ProjectedValue float,,
+    AveragePPG float
+);
