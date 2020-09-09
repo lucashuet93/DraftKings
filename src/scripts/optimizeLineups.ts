1bb4e871a772d80c5d8cf71ffd98e808f9a11b97
Lucas Huet-Hudson <lucas.huet.hudson@gmail.com>
2020-09-08 22:39:52 -0400

    write lineups to csv

--- src/scripts/optimizeLineups.ts
+++ src/scripts/optimizeLineups.ts
@@ -9,7 +9,7 @@ import {
   Timer,
 } from '../services';
 import { ProjectedPlayer, DraftKingsLineup } from '../models';
-
+import { createObjectCsvWriter } from 'csv-writer';
 // load environment variables
 config();
 
@@ -21,8 +21,22 @@ const optimizeLineups = async () => {
   const lineupOptimizer: LineupOptimizer = new LineupOptimizer(salaryAnalyzer);
   const lineupAnalyzer: LineupAnalyzer = new LineupAnalyzer();
   const timer: Timer = new Timer();
-
+  const csvWriter = createObjectCsvWriter({
+    path: 'out.csv',
+    header: [
+      { id: 'QB', title: 'QB' },
+      { id: 'RB1', title: 'RB' },
+      { id: 'RB2', title: 'RB' },
+      { id: 'WR1', title: 'WR' },
+      { id: 'WR2', title: 'WR' },
+      { id: 'WR3', title: 'WR' },
+      { id: 'TE', title: 'TE' },
+      { id: 'FLEX', title: 'FLEX' },
+      { id: 'DST', title: 'DST' },
+    ],
+  });
   timer.start();
+
   const rawPlayerProjections: ColumnValue[][] = await sqlServerService.loadRawPlayerData(
     'PlayerProjections'
   );
@@ -33,7 +47,9 @@ const optimizeLineups = async () => {
     projectedPlayers,
     150
   );
+
   lineupAnalyzer.printLineupStatistics(projectedPlayers, optimizedLineups);
+  await csvWriter.writeRecords(optimizedLineups);
   await sqlServerService.saveTopLineups(optimizedLineups, () => {
     const elapsedMilliseconds: number = timer.end();
     const elapsedTime: string = timer.getTimeStringFromMilliseconds(
